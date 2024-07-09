1279767081f08e4dd2b5eab5fe780453899553b1
Antonio Vuono <antoniosvuono@icloud.com>
2024-07-09 14:09:58 -0300

    feature: day 1 finalized

---
+++ src/components/input.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import { TextInput, View, TextInputProps, Platform } from "react-native";
+import clsx from "clsx";
+import { colors } from "@/styles/colors";
+
+type Variants =  'primary' | 'secondary' | 'tertiary'
+
+type InputProps = {
+    children: ReactNode
+    variants?: Variants
+}
+
+function Input({children, variants = 'primary'}: InputProps) {
+    return (
+        <View className={clsx(
+            "w-full h-16 flex-row items-center gap-2",
+            {
+                "h-14 px-4 rounded-lg border border-zinc-800" : variants !== 'primary',
+                "bg-zinc-950" : variants === 'secondary',
+                "bg-zinc-900" : variants === 'tertiary'
+            },
+        )}>
+            {children}
+        </View>
+    )
+}
+
+function Field({...rest}: TextInputProps) {
+   return <TextInput {...rest} className="flex-1 text-zinc-100 text-lg font-regular" placeholderTextColor={colors.zinc[400]} cursorColor={colors.zinc[100]} selectionColor={Platform.OS === 'ios' ? colors.zinc[100] : undefined}/>
+}
+
+Input.Field = Field;
+
+export { Input }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// Import tokens to customize theme
+import { colors } from './src/styles/colors'
+import { fontFamily } from './src/styles/fontFamily'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+      extend: {
+        colors,
+        fontFamily
+      },
+    },
+    plugins: [],
+  }
